6e2958584144e6dec6ad936ecded3600292b831f
braingremlin85 <triky.ch@gmail.com>
2026-08-23 17:16:21 +0200

    Fix: added 'irc_key' column to users table migration

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -37,6 +37,7 @@ class UserSeeder extends Seeder
                 'passkey'           => md5(random_bytes(60)),
                 'rsskey'            => md5(random_bytes(60)),
                 'api_token'         => Str::random(100),
+                'irc_key'           => Str::random(100),
             ],
             [
                 'id'                => 2,
@@ -48,6 +49,7 @@ class UserSeeder extends Seeder
                 'passkey'           => md5(random_bytes(60)),
                 'rsskey'            => md5(random_bytes(60)),
                 'api_token'         => Str::random(100),
+                'irc_key'           => Str::random(100),
             ],
             [
                 'id'                => 3,
@@ -59,6 +61,7 @@ class UserSeeder extends Seeder
                 'passkey'           => md5(random_bytes(60)),
                 'rsskey'            => md5(random_bytes(60)),
                 'api_token'         => Str::random(100),
+                'irc_key'           => Str::random(100),
             ],
         ], ['username'], ['updated_at' => DB::raw('updated_at')]);
     }
